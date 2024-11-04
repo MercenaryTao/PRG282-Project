08ccc60474d3ff7c5e3453fd0519108f60e18df3
Administrator <Administrator@MARS>
2024-11-04 16:11:12 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestPrep1.PresentationLayer;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Data.SqlClient;
+using TestPrep1.DataLayer;
+using System.Diagnostics;
+using System.Security.Cryptography;
+namespace TestPrep1
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        string filePath = @"C:\Users\Administrator\source\repos\PRG282 Project\dataFile.txt";
+        string rawDatafilePath = @"C:\Users\Administrator\source\repos\PRG282 Project\rawData.txt";
+        string dataLink = @"Data Source=MARS\SQLEXPRESS;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+
+        //SqlConnection conn = new SqlConnection();
+        public Form1()
+        {
+            InitializeComponent();
+            dataDisplay.AutoGenerateColumns = false;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            DataHandler handler = new DataHandler();
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void surnameInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)///adds to the list
+        {
+            try
+            {
+                
+                string sID = stuID.Text;
+                string name = stuName.Text;
+                string surname = stuSurname.Text;
+                string age = stuAge.Text;
+                string course = stuCourse.Text;
+                
+
+                Student student = new Student(sID, name, surname, age, course);
+                students.Add(student);
+
+                dataDisplay.Rows.Add(student.StudentID, student.Name, student.Surname, student.Age, student.Course);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+
+        }
+
+        private void saveToFile_Click(object sender, EventArgs e)///saves to the file
+        {
+            
+            using (StreamWriter sr = new StreamWriter(rawDatafilePath))
+            {
+                foreach (var item in students)
+                {
+                    sr.WriteLine(item);
+                }
+            }
+        }
+
+        private void readFromFile_Click(object sender, EventArgs e)
+        {
+ 
+            dataDisplay.Rows.Clear();
+
+            students.Clear();
+
+            try
+            {
+                // Read each line from the file
+                foreach (string line in File.ReadLines(rawDatafilePath))
+                {
+
+                    string[] data = line.Split(',');
+
+
+                    //string[] data = line.Split(',');
+
+                    // Ensure we have exactly 3 elements (Name, Surname, Job)
+
+                    if (string.IsNullOrWhiteSpace(line)) continue; // Skip empty or whitespace-only lines
+
+                    if (data.Length < 20)
+                    {
+                        
+
+                        // Trim each element to remove extra whitespace
+                        string sID = data[0];
+                        string name = data[1].Trim();
+                        string surname = data[2].Trim();
+                        string age = data[3].Trim();
+                        string course = data[4].Trim();
+
+
+                        // Create a new Person object
+                        Student student = new Student(sID, name, surname, age, course);
+
+
+                        // Add the Person object to the people list
+                        students.Add(student);
+
+
+
+                        // Add a row to the DataGridView for this person
+                        dataDisplay.Rows.Add(name, student.Surname, student.Age, student.Course);
+                    }
+                    else { MessageBox.Show(@"Catastrophic Failure",
+$"Processing line: {line}"); }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Display an error message if the file can't be read
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+
+        private void nameInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PresentationLayer/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPrep1.PresentationLayer
+{
+    internal class Student
+    {
+        public string StudentID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Age {  get; set; }
+        public string Course { get; set; }
+
+        public Student(string sID, string name, string surname, string age, string course)
+        {
+            this.StudentID = sID;
+                this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.Course = course;
+        }
+
+        public override string ToString()
+        {
+            return $"{StudentID} {Name} {Surname} {Age} {Course}";
+           
+        }
+
+        public string rawData()
+        {
+            return $"Student ID: {StudentID} | Name: {Name} | Surname: {Surname} | Age: {Age} | Course Enrolled: {Course}";
+        }
+           
+
+    }
+}
